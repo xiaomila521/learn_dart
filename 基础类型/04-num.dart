1cf7ffbdb6883b33fa4040533108c2e7db033c34
szp <1519297645@qq.com>
2025-01-22 14:24:07 +0800

    add:num

---
+++ 基础类型/04-num.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  // 数值类型
+  // int a = 1;
+  // double b = 1.1;
+  // num c = 1; // num 是 int 和 double 的父类
+
+  // 表示法
+  // int a = 100; // 十进制
+  // int b = 0xFF; // 十六进制
+  // num c = 2e3; // 科学表示法
+
+  // 数值转换
+
+  int a = int.parse("12312");
+  String b = 21321.toString();
+  double c = double.parse("123.123");
+}
